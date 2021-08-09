931a66eb41b4225ebf4f60d230be6ffb2188ce35
unknown <nhathau196@gmail.com>
2021-08-09 15:15:03 +0700

    add file

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51vSbWpF+dS._AC_SX184_.jpg',
+    title: 'I love You to the moon and back',
+    author: 'Amela hepworth',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51ARr3cay-L._AC_SX184_.jpg',
+    title: 'Razorblade Tears: A Novel',
+    author: 'S. A. Cosby ',
+  },
+]
