d485b83cb231445050d72c4137c62d318fe8cb75
unknown <nhathau196@gmail.com>
2021-10-27 16:15:30 +0700

    use Values library and array ...arr

--- react-projects/09-color-generator/setup/src/App.js
+++ react-projects/09-color-generator/setup/src/App.js
@@ -4,7 +4,44 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [value, setColor] = useState('')
+  // #f15025
+  const [list, setList] = useState(new Values('#f15025').all(10))
+  const [error, seterror] = useState(false)
+
+  const handleColor = (e) => {
+    e.preventDefault()
+    if (!/^#[0-9A-F]{6}$/i.test(value)) {
+      seterror(true)
+    } else {
+      setList(new Values(value).all(10))
+    }
+  }
+
+  return (
+    <section>
+      <section className='container'>
+        <h3>Color Generator</h3>
+        <form onSubmit={handleColor}>
+          <input
+            className={!error ? '' : 'error'}
+            type='text'
+            placeholder='#f15025'
+            value={value}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, i) => {
+          return <SingleColor {...color} key={i} index={index} />
+        })}
+      </section>
+    </section>
+  )
 }
 
 export default App
--- react-projects/09-color-generator/setup/src/SingleColor.js
+++ react-projects/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight }) => {
+  const [alert, setAlert] = useState(false)
+
+  const coppyClipBoard = () => {
+    navigator.clipboard.writeText(rgbToHex(...rgb))
+    setAlert(true)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+  }, [alert])
+  return (
+    <div
+      className='color'
+      style={{ backgroundColor: 'rgb(' + rgb.join(',') + ')' }}
+      onClick={coppyClipBoard}
+    >
+      <p className='percent-value'>{rgbToHex(...rgb)}</p>
+      <p className='color-value'>{weight}%</p>
+      {alert && <p className='alert'>COPIED TO CLIPBOARD</p>}
+    </div>
+  )
 }
 
 export default SingleColor
