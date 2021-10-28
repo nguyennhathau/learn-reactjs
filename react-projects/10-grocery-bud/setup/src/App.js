43a31ad379673fe87daeb6dc293862b5d07e9a34
unknown <nhathau196@gmail.com>
2021-10-29 01:38:00 +0700

    edit array in state with condition in arr.map, check display list.length>0 && content in render

--- react-projects/10-grocery-bud/setup/src/App.js
+++ react-projects/10-grocery-bud/setup/src/App.js
@@ -3,7 +3,100 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState('')
+  const [alert, setAlert] = useState({ show: false, type: '', msg: '' })
+
+  const inputBug = (e) => {
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value')
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item === editID) {
+            return name
+          }
+          return item
+        })
+      )
+      setName('')
+      setIsEditing(false)
+      setEditID('')
+      showAlert(true, 'success', 'value changed')
+    } else {
+      showAlert(true, 'success', 'item added to the list')
+      setList([...list, name])
+      setName('')
+    }
+    e.preventDefault()
+  }
+
+  const inputBtn = (name) => {
+    const specificItem = list.find((item) => item === name)
+    setIsEditing(true)
+    setEditID(name)
+    setName(specificItem)
+  }
+  const deleteBtn = (bug) => {
+    let newArr = list.filter((x) => x !== bug)
+    setList(newArr)
+    setName('')
+    showAlert(true, 'danger', 'item removed')
+  }
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg })
+  }
+
+  const deleteAll = () => {
+    showAlert(true, 'danger', 'Empty List')
+    setList([])
+  }
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setAlert({ ...alert, show: false })
+    }, 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [alert])
+
+  return (
+    <main>
+      <section className=' section section-center'>
+        {alert.show && <Alert {...alert} />}
+        <div className='grocery-form'>
+          <h3>Grocery Bug</h3>
+          <form action='#' onSubmit={inputBug} className='form-control'>
+            <input
+              type='text'
+              name='bug'
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value)
+              }}
+              className='grocery'
+            />
+            <button type='submit' className='submit-btn'>
+              {isEditing ? 'edit' : 'submit'}
+            </button>
+          </form>
+        </div>
+        {list.length > 0 && (
+          <div className='grocery-container'>
+            <List list={list} inputBtn={inputBtn} deleteBtn={deleteBtn} />
+
+            <button className='clear-btn' onClick={deleteAll}>
+              Clear Items
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
+  )
 }
 
 export default App
--- react-projects/10-grocery-bud/setup/src/List.js
+++ react-projects/10-grocery-bud/setup/src/List.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({ list, inputBtn, deleteBtn }) => {
+  return (
+    <div className=' grocery-container'>
+      {list.map((bug, i) => {
+        return (
+          <div className=' grocery-item' key={i}>
+            <p className='title'>{bug}</p>
+            <div>
+              <button className='edit-btn' onClick={() => inputBtn(bug)}>
+                <FaEdit />
+              </button>
+              <button className='delete-btn' onClick={() => deleteBtn(bug)}>
+                <FaTrash />
+              </button>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default List
