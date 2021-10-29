7c24a71229252f33c183042c23174ec44230d2e8
unknown <nhathau196@gmail.com>
2021-10-29 22:59:45 +0700

    use  class=`{toggle && nameClass}` and useRef

--- react-projects/11-navbar/setup/src/Navbar.js
+++ react-projects/11-navbar/setup/src/Navbar.js
@@ -4,7 +4,39 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <div className='nav-center'>
+      <div className='nav-header'>
+        <img src={logo} alt='' className='logo' />
+        <button className='nav-toggle' onClick={() => setToggle(!toggle)}>
+          <FaBars />
+        </button>
+      </div>
+
+      <div className={`links-container links ${toggle && 'show-container1'}`}>
+        {links.map((item) => {
+          const { id, url, text } = item
+          return (
+            <a href={url} key={id}>
+              {text}
+            </a>
+          )
+        })}
+      </div>
+      <div className='social-icons'>
+        {social.map((item) => {
+          const { id, url, icon } = item
+          return (
+            <a href={url} key={id}>
+              {icon}
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
