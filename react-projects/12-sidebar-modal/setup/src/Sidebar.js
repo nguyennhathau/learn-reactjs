347f2e044c5de55cdcc0dac091c00457eab60bcf
unknown <nhathau196@gmail.com>
2021-10-30 23:33:44 +0700

    12 -sidebar

--- react-projects/12-sidebar-modal/setup/src/Sidebar.js
+++ react-projects/12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaBars } from 'react-icons/fa'
 import { social, links } from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  const { isSideBarOpen, closeSideBar } = useGlobalContext()
+  return (
+    <aside className={`${isSideBarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+      <div className='sidebar-header'>
+        <img src={logo} alt='' className='logo' />
+        <button className='close-btn' onClick={closeSideBar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+
+      <ul className='social-icons'>
+        {social.map((item) => {
+          const { id, url, icon } = item
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
 }
 
 export default Sidebar
--- react-projects/12-sidebar-modal/setup/src/context.js
+++ react-projects/12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,47 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openSideBar = () => {
+    console.log('open sidebar')
+    setIsSideBarOpen(true)
+  }
+
+  const closeSideBar = () => {
+    setIsSideBarOpen(false)
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        // add nhiều đối tượng dùng {}
+        isModalOpen,
+        isSideBarOpen,
+        openSideBar,
+        closeSideBar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
