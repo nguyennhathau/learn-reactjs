950d9d6e929c081e84e9f188e4f4ffd2d48aa2a4
unknown <nhathau196@gmail.com>
2021-10-31 22:56:24 +0700

    13-stripe-submenus use getboundingClientRect()

--- react-projects/13-stripe-submenus/setup/src/Sidebar.js
+++ react-projects/13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,42 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const { isSideBarOpen, closeSidebar } = useGlobalContext()
+
+  return (
+    <div
+      className={`${
+        isSideBarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+      }`}
+    >
+      <aside className='sidebar'>
+        {sublinks.map((item, i) => {
+          const { page, links } = item
+          return (
+            <article key={i}>
+              <h4>{page}</h4>
+              <div className='sidebar-sublinks'>
+                {links.map((link, index) => {
+                  const { label, icon, url } = link
+                  return (
+                    <a href={url} key={index}>
+                      {icon} {label}
+                    </a>
+                  )
+                })}
+              </div>
+            </article>
+          )
+        })}
+        <button className='btn close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </aside>
+    </div>
+  )
 }
 
 export default Sidebar
