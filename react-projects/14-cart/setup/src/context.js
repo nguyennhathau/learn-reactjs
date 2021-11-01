adcf797df71b7d3892faec2f414265414d593228
unknown <nhathau196@gmail.com>
2021-11-01 23:19:21 +0700

    14-cart use useReducer

--- react-projects/14-cart/setup/src/context.js
+++ react-projects/14-cart/setup/src/context.js
@@ -6,13 +6,60 @@ import reducer from './reducer'
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
+const initialState = {
+  loadding: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+}
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  // const [cart, setCart] = useState(cartItems)
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const increate = (id) => {
+    dispatch({ type: 'INCREMENT', payload: id })
+  }
+
+  const decrease = (id) => {
+    dispatch({ type: 'DECREMENT', payload: id })
+  }
+
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id })
+  }
+
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_ALL' })
+  }
+
+  // const fetchData = async () => {
+  //   dispatch({ type: 'LOADING' })
+  //   const response = await fetch(url)
+  //   const cart = await response.json()
+  //   dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
+  // }
+  // const toggleAmount = (id, type) => {
+  //   dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } })
+  // }
+  // useEffect(() => {
+  //   fetchData()
+  // }, [])
+
+  useEffect(() => {
+    dispatch({ type: 'Get-Total-Amount' })
+  }, [state.cart])
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
+        increate,
+        decrease,
+        remove,
+        clearCart,
+        // toggleAmount,
       }}
     >
       {children}
