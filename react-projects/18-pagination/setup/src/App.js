069b26782882d4389eebf05ba08490d132d7ba7d
unknown <nhathau196@gmail.com>
2021-11-05 22:29:42 +0700

    18-pagination update

--- react-projects/18-pagination/setup/src/App.js
+++ react-projects/18-pagination/setup/src/App.js
@@ -2,7 +2,69 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+
+  const preClick = () => {
+    if (page === 0) {
+      return
+    }
+    setPage(page - 1)
+  }
+
+  const nextClick = () => {
+    if (page === data.length - 1) {
+      return
+    }
+    setPage(page + 1)
+  }
+
+  const handleClick = (index) => {
+    setPage(index)
+  }
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page])
+
+  return (
+    <main>
+      <section className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+      </section>
+      <div className='followers'>
+        <div className='container'>
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />
+          })}
+        </div>
+      </div>
+      {!loading && (
+        <div className='btn btn-container'>
+          <button onClick={preClick} className='prev-btn'>
+            prev
+          </button>
+          {data.map((item, index) => {
+            return (
+              <button
+                onClick={() => handleClick(index)}
+                key={index}
+                className={`page-btn ${index === page ? 'active-btn' : null}`}
+              >
+                {index + 1}
+              </button>
+            )
+          })}
+          <button onClick={nextClick} className='prev-btn'>
+            next
+          </button>
+        </div>
+      )}
+    </main>
+  )
 }
 
 export default App
