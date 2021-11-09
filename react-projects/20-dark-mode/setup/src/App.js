6c43c1c597096833bb5d9027bd65749712809fdc
unknown <nhathau196@gmail.com>
2021-11-09 14:16:05 +0700

    20-dark-mode

--- react-projects/20-dark-mode/setup/src/App.js
+++ react-projects/20-dark-mode/setup/src/App.js
@@ -2,8 +2,50 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const light_theme = 'light-theme'
+const dark_theme = 'dark-theme'
+
+const getLocalStorage = () => {
+  let themeLocal = light_theme
+  if (localStorage.getItem('theme')) {
+    themeLocal = localStorage.getItem('theme')
+  }
+  return themeLocal
+}
+
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getLocalStorage())
+
+  const handleClick = () => {
+    if (theme === light_theme) {
+      setTheme(dark_theme)
+    } else {
+      setTheme(light_theme)
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme)
+    document.getElementsByTagName('body')[0].className = theme
+    //document.documentElement.className = theme;
+  }, [theme])
+  return (
+    <main>
+      <section className='section'>
+        <div className='nav-center'>
+          <h1>Overreacted</h1>
+          <button className='btn' onClick={handleClick}>
+            Toggle
+          </button>
+        </div>
+        <div className='articles'>
+          {data.map((item, index) => {
+            return <Article key={index} {...item} />
+          })}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App
--- react-projects/20-dark-mode/setup/src/Article.js
+++ react-projects/20-dark-mode/setup/src/Article.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+const Article = ({ title, date, snippet, length }) => {
+  const formatDate = moment(date).format('dddd Do, YYYY')
+  return (
+    <div className='post'>
+      <h2>{title}</h2>
+      <p className='post-info'>
+        {formatDate} {length} min read
+      </p>
+      <p>{snippet}</p>
+    </div>
+  )
 }
 
 export default Article
