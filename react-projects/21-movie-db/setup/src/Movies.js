11f4b3224359881304c97e3aaf9228b21146bb05
unknown <nhathau196@gmail.com>
2021-11-12 14:40:40 +0700

    21-movie-db update context.js

--- react-projects/21-movie-db/setup/src/Movies.js
+++ react-projects/21-movie-db/setup/src/Movies.js
@@ -5,34 +5,36 @@ const url =
   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
 const Movies = () => {
-  const { isLoading, movies } = useGlobalContext()
+  const { isLoading, data: movies } = useGlobalContext()
+
   if (isLoading) {
+    alert('loading')
     return <div className='loading'></div>
   }
 
+  alert('movies')
   return (
     <section className='movies'>
-      {movies &&
-        movies.map((movie) => {
-          const {
-            Title: title,
-            Year: year,
-            imdbID: id,
-            Type: type,
-            Poster: poster,
-          } = movie
-          return (
-            <Link to={`/movies/${id}`} key={id} className='movie'>
-              <article>
-                <img src={poster === 'N/A' ? url : poster} alt={title} />
-                <div className='movie-info'>
-                  <h4 className='title'>{title}</h4>
-                  <p>{year}</p>
-                </div>
-              </article>
-            </Link>
-          )
-        })}
+      {movies.map((movie) => {
+        const {
+          Title: title,
+          Year: year,
+          imdbID: id,
+          Type: type,
+          Poster: poster,
+        } = movie
+        return (
+          <Link to={`/movies/${id}`} key={id} className='movie'>
+            <article>
+              <img src={poster === 'N/A' ? url : poster} alt={title} />
+              <div className='movie-info'>
+                <h4 className='title'>{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        )
+      })}
     </section>
   )
 }
--- react-projects/21-movie-db/setup/src/context.js
+++ react-projects/21-movie-db/setup/src/context.js
@@ -1,17 +1,42 @@
 import React, { useState, useContext, useEffect } from 'react'
-import useFetch from './useFetch'
 // make sure to use https
 // export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=e1aaed3`
-
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
   const [query, setQuery] = useState('batman')
-  const { isLoading, error, data: movies } = useFetch(`&s=${query}`)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState({ show: false, msg: '' })
+  const [data, setData] = useState(null)
 
+  let urlParams = `&s=${query}`
+
+  const fetchData = async (url) => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+
+      if (data.Response === 'True') {
+        setData(data.Search || data)
+
+        setError({ show: false, msg: '' })
+      } else {
+        setError({ show: true, msg: data.Error })
+      }
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    alert('context')
+    fetchData(`${API_ENDPOINT}${urlParams}`)
+  }, [urlParams])
   return (
-    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+    <AppContext.Provider value={{ isLoading, data, error, query, setQuery }}>
       {children}
     </AppContext.Provider>
   )
